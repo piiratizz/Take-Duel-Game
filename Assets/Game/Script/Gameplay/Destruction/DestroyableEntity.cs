1865792596f4aa48425e579c8bac927734bf8f6a
piiratizz <84204753+piiratizz@users.noreply.github.com>
2025-07-03 20:15:57 +0300

    Destruction update
    Added destroyable objects

---
+++ Assets/Game/Script/Gameplay/Destruction/DestroyableEntity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+[RequireComponent(typeof(NetworkIdentity), typeof(Rigidbody))]
+public class DestroyableEntity : NetworkBehaviour, IDestroyable
+{
+    [SerializeField] private GameObject defaultObject;
+    [SerializeField] private GameObject celledObject;
+    [SerializeField] private float objectMass;
+    
+    private NetworkIdentity _cachedIdentityComponent;
+    private Rigidbody _cachedRigidBodyComponent;
+    private Collider _cachedMainColliderComponent;
+    
+    private const float ForceMultiplier = 1000f;
+    private const float RawRadius = 100f;
+    
+    private void Awake()
+    {
+        _cachedIdentityComponent = GetComponent<NetworkIdentity>();
+        _cachedRigidBodyComponent = GetComponent<Rigidbody>();
+        _cachedMainColliderComponent = GetComponent<Collider>();
+        
+        celledObject.SetActive(false);
+        defaultObject.SetActive(true);
+    }
+
+    public void PerformHit(HitContext ctx)
+    {
+        DestroyObject(ctx);
+    }
+    
+    [ClientRpc]
+    private void DestroyObject(HitContext ctx)
+    {
+        defaultObject.SetActive(false);
+        celledObject.SetActive(true);
+
+        _cachedMainColliderComponent.enabled = false;
+        _cachedRigidBodyComponent.AddExplosionForce(ctx.BulletDamage * ForceMultiplier, transform.position, RawRadius / objectMass);
+    }
+    
+    public NetworkIdentity GetNetworkIdentity()
+    {
+        return _cachedIdentityComponent;
+    }
+}
